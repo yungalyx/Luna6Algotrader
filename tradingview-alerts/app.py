c3f5f4b07cc68f820bc91f731c8ea6338806d4ef
yungalyx <52767711+yungalyx@users.noreply.github.com>
2020-09-06 22:33:28 -0700

    configured env vars, changed market order into bracket order with take profit and stop loss, used finnhub to generate price targets

--- tradingview-alerts/app.py
+++ tradingview-alerts/app.py
@@ -9,11 +9,17 @@ ORDER_URL = '{}/v2/orders'.format(ENDPOINT_URL)
 CLOCK_URL = '{}/v2/clock'.format(ENDPOINT_URL)
 DATA_URL = 'https://data.alpaca.markets/v1'
 HEADERS = {'APCA-API-KEY-ID': os.environ['API_KEY'], 'APCA-API-SECRET-KEY': os.environ['SECRET_KEY']}
+FINNHUBHEADER = {'X-Finnhub-Token': os.environ['FINNHUBKEY']}
 
 app = Chalice(app_name='luna6trade')
 
 
-# print(os.environ['API_KEY'])
+def test1():
+    stonks = ['AAPL', 'RACE', 'TSLA', 'QQQ']
+    for ticker in stonks:
+        r = requests.get('https://finnhub.io/api/v1/quote?symbol={}'.format(ticker), headers=FINNHUBHEADER)  # or header
+        print(r.json())
+
 
 def get_alpaca_account():
     r = requests.get(ACCOUNT_URL, headers=HEADERS)
@@ -21,6 +27,28 @@ def get_alpaca_account():
 
 
 print(get_alpaca_account())
+
+#
+
+def create_bracket_order(symbol, qty, side, type, limit, stop):
+    data = {
+        "symbol": symbol,
+        "qty": qty,
+        "side": side,
+        "type": type,
+        "time_in_force": 'day',
+        'order_class':'bracket',
+        'take_profit': {
+            'limit_price': limit
+        },
+        'stop_loss': {
+            'stop_price': stop
+        }
+    }
+    r = requests.post(ORDER_URL, json=data, headers=HEADERS)
+    return json.loads(r.content)
+
+
 
 
 def create_order(symbol, qty, side, type, time_in_force):
@@ -56,9 +84,20 @@ def get_account():
 
 @app.schedule(Rate(1, unit=Rate.HOURS))
 def every_min(event):
+
     r = requests.get(CLOCK_URL, headers=HEADERS)
     marketclock = json.loads(r.content)
 
     if marketclock["is_open"]:
         # trading logic
-        create_order("MSFT", 1, "buy", "market", "gtc")
+        quote = requests.get('https://finnhub.io/api/v1/quote?symbol=AAPL', headers=FINNHUBHEADER)
+        current_price = quote.json()
+        curr = (current_price['c'])
+
+        target = requests.get('https://finnhub.io/api/v1/stock/price-target?symbol=AAPL', headers=FINNHUBHEADER)
+        target_price = target.json()
+        shouldbe = (target_price['targetHigh'] + target_price['targetMedian'])/2
+
+        # STOP LOSS 5% below and Take profit 5% above
+        if shouldbe > curr:
+            create_bracket_order('AAPL', 1, 'buy', 'market', limit=shouldbe, stop=curr*0.93)
